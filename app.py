99252ee6399c8dd1864e6b1ca39f16701a2297bf
LiPakLim2526 <250392775@stu.vtc.edu.hk>
2026-04-16 23:27:07 +0800

    Complete full-stack integration: MySQL to Flask frontend
    Confirmed database connectivity and dynamic rendering of video cards on the index page.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL  
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'rootroot' 
+app.config['MYSQL_DB'] = 'tvb_db'
+
+mysql = MySQL(app) 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor() 
+    cur.execute("SELECT title, category, image_url FROM videos")
+    video_list = cur.fetchall()
+    cur.close()
+    return render_template('index.html', videos=video_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
